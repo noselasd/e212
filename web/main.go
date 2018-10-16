9c9a491c9ba457ea1381ce72bb18aae4bbc480bf
Nils O. Selåsdal <noselasd@fiane.mooo.com>
2018-10-16 21:41:20 +0200

    Add flags for controlling http port, etc.

--- web/main.go
+++ web/main.go
@@ -4,6 +4,11 @@ import (
 	"e212/routes"
 	"e212/store"
 	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
 	//"github.com/go-macaron/macaron"
 	"github.com/go-macaron/session"
@@ -68,7 +73,36 @@ func loginPost(ctx *routes.AppContext) {
 	ctx.Redirect("/")
 }
 
+var gPort = flag.Int("port", 4000, "port number to listen on")
+var gUseTLS = flag.Bool("usetls", false, "Use TLS(HTTPS) intead of plain HTTP")
+var gTLSCert = flag.String("tlscert", "tls.cert", "Path to TLS certificate file")
+var gTLSKey = flag.String("tlskey", "tls.key", "Path to TLS key file")
+
+func runServer(r *macaron.Macaron) {
+
+	srv := http.Server{
+		Addr:         fmt.Sprintf(":%d", *gPort),
+		Handler:      r,
+		ReadTimeout:  45 * time.Second,
+		WriteTimeout: 45 * time.Second,
+	}
+
+	log.Printf("listening on %s\n", srv.Addr)
+
+	var err error
+	if *gUseTLS {
+		err = srv.ListenAndServeTLS(*gTLSCert, *gTLSKey)
+	} else {
+		err = srv.ListenAndServe()
+	}
+	if err != nil {
+		log.Fatalln("ListenAndServe: ", err)
+	}
+
+}
+
 func main() {
+	flag.Parse()
 	err := store.Init("mccmnc.db")
 	if err != nil {
 		panic(err)
@@ -89,5 +123,6 @@ func main() {
 	r.Get("/login", loginGet)
 	r.Post("/login", loginPost)
 	r.Post("/logout", logout)
-	r.Run()
+
+	runServer(r)
 }
